315a209cd99b90d2e5445a348004ae58e6742148
Titus Iudean <iudeant@gmail.com>
2016-08-10 13:03:19 +0300

    Completes questions 2 and 3

--- index.js
+++ index.js
@@ -6,14 +6,48 @@ $.get("https://async-workshops-api.herokuapp.com/people", function(peopleRespons
   });
 });
 
+// QUESTION 1
+// var clickEvent;
+// console.log(1);
+//
+// $(document).click(function(event) {
+//   console.log(3);
+//   clickEvent = event;
+//   console.log("Mouse click event object:", clickEvent);
+//   console.log(4);
+// });
+// console.log(2);
+// QUESTION 1 END
 
-var clickEvent;
+
+// // QUESTION 2
+// $.get("https://async-workshops-api.herokuapp.com/people", function(peopleResponse) {
+//   console.log(1);
+//   console.log(peopleResponse);
+//   console.log(2);
+// });
+
+
+
+// QUESTION 3
+var people;
 console.log(1);
-
-$(document).click(function(event) {
+$.get("https://async-workshops-api.herokuapp.com/people", function(peopleResponse) {
   console.log(3);
-  clickEvent = event;
-  console.log("Mouse click event object:", clickEvent);
+  people = peopleResponse;
   console.log(4);
+
 });
-console.log(2);
+
+setTimeout(
+  function()
+  {
+    //do something special
+
+    console.log(people);
+    console.log("done");
+    console.log(2);
+  }, 5000);
+
+
+// QUESTION 3 END
